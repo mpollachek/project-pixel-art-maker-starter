2f1890da6a45ac6ae3435a09389bb2d40a44f707
mpollachek <mpollachek81@gmail.com>
2022-07-14 14:58:13 -0400

    Udacity pixel art maker project

--- designs.js
+++ designs.js
@@ -1,10 +1,47 @@
-// Select color input
-// Select size input
+const makeGrid = (h, w) => {
+  const myDiv = document.createElement("div");
+  setWidth = 22 * w;
+  myDiv.setAttribute("id", "myDiv");
+  myDiv.style.width = `${setWidth}px`;
+  myDiv.style.margin = "auto";
+  for (let r = 1; r <= h; r++) {
+    const newRow = document.createElement("tr");
+    console.log("tr - " + h);
+    myDiv.appendChild(newRow);
+    for (let c = 1; c <= w; c++) {
+      const newCol = document.createElement("td");
+      newCol.addEventListener("click", function (e) {
+        let target = e.target;
+        if (target.classList.contains("selected")) {
+          target.classList.remove("selected");
+        } else {
+          target.classList.add("selected");
+        }
+      });
+      console.log("td - " + w);
+      newRow.appendChild(newCol);
+    }
+  }
+  document.body.appendChild(myDiv);
+};
 
-// When size is submitted by the user, call makeGrid()
+const size = document.querySelector("#sizePicker");
+size.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const h = document.getElementById("inputHeight").value;
+  const w = document.getElementById("inputWidth").value;
+  const divToRemove = document.querySelector("#myDiv");
+  if (divToRemove) {
+    divToRemove.remove();
+  }
+  makeGrid(h, w);
+});
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+const colorSelected = document.querySelector("#colorPicker");
+colorSelected.addEventListener("input", function () {
+  const boxSelected = document.querySelectorAll(".selected");
+  boxSelected.forEach(function (b) {
+    b.style.backgroundColor = colorSelected.value;
+    b.classList.remove("selected");
+  });
+});
